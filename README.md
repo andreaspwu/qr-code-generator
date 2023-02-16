f9d752fe77249438db2848e0f94e95a0bebdcad5
andreaspwu <lhart7077@gmail.com>
2023-02-16 17:58:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# QRcode-Generator
+
+React QRcode Generator by Ifeanyi Omeata
+
+Demo: https://qrcode-ifeanyi.vercel.app/ <br>
+Watch on Youtube: https://youtu.be/7oITMoDR4Us <br>
+Website: https://ifeanyiomeata.com <br>
+Tech Stack: React, Javascript, TailwindCSS, Chat-GPT, Vercel <br>
+
+<img width="1440" alt="p-qrcode" src="https://user-images.githubusercontent.com/32337103/209725778-93352d46-9357-40f0-b8fe-a51cfb78bb6f.png">
+
+
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import QRCode from 'qrcode.react'
+
+function App() {
+  const [url, setUrl] = useState('https://www.google.com')
+  const [fileType, setFiletype] = useState('png')
+
+  const handleDownload = (e) => {
+    const canvas = e.target
+    const imageData = canvas.toDataURL(`image/${fileType}`)
+    const link = document.createElement('a')
+    link.download = `my-image.${fileType}`
+    link.href = imageData
+    link.click()
+  }
+
+  const handleChange = (e) => {
+    setUrl(e.target.value)
+  }
+
+  const handleChoice = (e) => {
+    setFiletype(e.target.value)
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-start m-16 text-center w-[80vw] mx-auto">
+      <h1 className="text-4xl font-extrabold text-white mb-8">
+        QR Code Generator
+      </h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="qr" className="hidden">
+          Enter QR Code Data
+        </label>
+        <input
+          className="mb-6 p-2 w-[80vw] min-[500px]:w-[60vw] min-w-full text-lg text-gray-500 outline-none border-4 rounded-lg text-center hover:border-gray-500"
+          onChange={handleChange}
+          type="text"
+          id="qr"
+          placeholder="http://www.google.com"
+        />
+      </form>
+      <div className="m-6 bg-white border-4 hover:border-8 rounded-lg border-gray-900 p-4">
+        <QRCode
+          onClick={handleDownload}
+          value={url}
+          size={200}
+          fgColor="#000000"
+          bgColor="#FFFFFF"
+        />
+      </div>
+      <p className="text-2xl font-light text-white mb-2">
+        Click the QR code to download it
+      </p>
+      <select
+        onChange={handleChoice}
+        className="py-2 px-8 font-bold outline-none rounded-md"
+      >
+        <option value="png">PNG</option>
+        <option value="jpeg">JPEG</option>
+        <option value="jpg">JPG</option>
+        <option value="gif">GIF</option>
+      </select>
+    </div>
+  )
+}
+
+export default App
